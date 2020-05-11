ef366f9e2fe89160ca5d17195b96ad5e7d797006
tavrora <tavrora@github.com>
2020-05-11 07:41:01 +0300

    Переход на страницу авторизации вынесен в отдельный метод

--- test_main_page.py
+++ test_main_page.py
@@ -1,8 +1,13 @@
 import time
 
-def test_guest_can_go_to_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
-    browser.get(link)
-    time.sleep(2)
+link = "http://selenium1py.pythonanywhere.com/"
+
+def go_to_login_page(browser):
     login_link = browser.find_element_by_css_selector("#login_link")
     login_link.click()
+
+def test_guest_can_go_to_login_page(browser):
+    browser.get(link)
+    time.sleep(2)
+    go_to_login_page(browser)
+    time.sleep(2)
