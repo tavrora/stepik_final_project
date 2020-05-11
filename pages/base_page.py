023de5bf300e42df576ef08e108e2479a634b655
tavrora <tavrora@github.com>
2020-05-11 13:33:37 +0300

    Первый тест на основе Page Object

--- pages/base_page.py
+++ pages/base_page.py
@@ -5,5 +5,4 @@ class BasePage():
         self.url = url
 
     def open(self):
-    	self.browser.get(self.url)
-    	
+        self.browser.get(self.url)
--- test_main_page.py
+++ test_main_page.py
@@ -1,13 +1,9 @@
 import time
+from .pages.main_page import MainPage
 
 link = "http://selenium1py.pythonanywhere.com/"
 
-def go_to_login_page(browser):
-    login_link = browser.find_element_by_css_selector("#login_link")
-    login_link.click()
-
 def test_guest_can_go_to_login_page(browser):
-    browser.get(link)
-    time.sleep(2)
-    go_to_login_page(browser)
-    time.sleep(2)
+    page = MainPage(browser, link)
+    page.open()
+    page.go_to_login_page()
