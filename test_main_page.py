5d066eb2384cd82b45f90f5c70364be139a51324
tavrora <tavrora@github.com>
2020-05-11 07:15:53 +0300

    Добавлен conftest.py и первый тест

---
+++ test_main_page.py
@@ -0,0 +1,8 @@
+import time
+
+def test_guest_can_go_to_login_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    browser.get(link)
+    time.sleep(2)
+    login_link = browser.find_element_by_css_selector("#login_link")
+    login_link.click()
