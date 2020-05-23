c5769d0629778586daad8aa34f76ddcc862b4bef
tavrora <tavrora@github.com>
2020-05-23 12:05:54 +0300

    Улучшено оформление параметризации

--- test_product_page.py
+++ test_product_page.py
@@ -23,20 +23,15 @@ class TestUserAddToBasketFromProductPage():
         # Проверить, что пользователь залогинен
         login_page.should_be_authorized_user()
 
+    @pytest.mark.param
     @pytest.mark.need_review
-    @pytest.mark.parametrize('link',
-                             ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
-                              "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
-                              "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2",
-                              "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer3",
-                              "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer4",
-                              "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer5",
-                              "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer6",
-                              pytest.param("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer7",
-                                           marks=pytest.mark.xfail),
-                              "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8",
-                              "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"])
-    def  test_user_can_add_product_to_basket(self, browser, link):
+    @pytest.mark.parametrize('link_part', ["?promo=offer0", "?promo=offer1", "?promo=offer2",
+                                           "?promo=offer3", "?promo=offer4", "?promo=offer5",
+                                           "?promo=offer6",
+                                           pytest.param("?promo=offer7", marks=pytest.mark.xfail),
+                                           "?promo=offer8", "?promo=offer9"])
+    def  test_user_can_add_product_to_basket(self, browser, link_part):
+        link = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/{link_part}"
         # Открыть страницу
         product_page = ProductPage(browser, link)
         product_page.open()
